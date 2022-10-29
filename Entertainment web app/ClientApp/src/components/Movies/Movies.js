94aca941612c92f472e283e600847c2e2fc2f427
Dawid Kłos <klosdawid12@gmail.com>
2022-10-29 16:03:42 +0100

    Finish styling Movies component

--- Entertainment web app/ClientApp/src/components/Movies/Movies.js
+++ Entertainment web app/ClientApp/src/components/Movies/Movies.js
@@ -1,7 +1,17 @@
+import './Movies.scss'
+import Card from '../Card/Card'
 
 const Movies = () => {
     return(
-        <></>
+        <div className="movies">
+            <h1 className="movies__title">Movies</h1>
+            <div className="movies__content">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
     )
 }
 
