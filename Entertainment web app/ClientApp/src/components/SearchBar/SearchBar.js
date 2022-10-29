281fd8c0c365a5f0126fae448d24a0610d61b164
Dawid Kłos <klosdawid12@gmail.com>
2022-10-29 09:03:56 +0100

    Add styles for SearchBar component and fix issues with font variables in _variables.scss

--- Entertainment web app/ClientApp/src/components/SearchBar/SearchBar.js
+++ Entertainment web app/ClientApp/src/components/SearchBar/SearchBar.js
@@ -2,9 +2,14 @@ import "./SearchBar.scss";
 
 const SearchBar = () => {
 
-    return(
-        <>
-        </>
+    return (
+        <div className="searchBar">
+            <img className="searchBar__icon" src="./assets/icon-search.svg" alt="Search icon"/>
+            <form className="searchBar__search">
+                <label for="search" className="visually-hidden">Search for movies or TV series</label>
+                <input className="searchBar__search--input" type="text" id="search" placeholder="Search for movies or TV series"/>
+            </form>
+        </div>
     )
 }
 
