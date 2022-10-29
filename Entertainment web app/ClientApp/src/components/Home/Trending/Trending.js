305a3a5fc04faf7e3488b0f87c96240436a9cdee
Dawid Kłos <klosdawid12@gmail.com>
2022-10-29 10:12:04 +0100

    Finish styling single card Component

--- Entertainment web app/ClientApp/src/components/Home/Trending/Trending.js
+++ Entertainment web app/ClientApp/src/components/Home/Trending/Trending.js
@@ -1,19 +1,13 @@
 import "./Trending.scss";
+import Card from './Card/Card';
+
 
 const Trending = () => {
     
     return(
-        <div className="card">
-            {/*<span className="card__bookmark">*/}
-            {/*    <img src="./assets/thumbnails/1998/trending/large.jpg" alt="asd" />*/}
-            {/*</span>*/}
-            <div className="card__info">
-                <p className="card__info--year">2019</p>
-                <p className="card__info--category">Movie</p>
-                <p className="card__info--rating">PG</p>
-            </div>
-            <h2 className="card__title">Panie, działa!</h2>
-            
+        <div className="container">
+            <h1 className="container__title">Trending</h1>
+            <Card />
         </div>
     )
 }
