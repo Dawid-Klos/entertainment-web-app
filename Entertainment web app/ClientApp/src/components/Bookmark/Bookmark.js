ffd1ce2adc9bd9ea6f2471974033be3d3dd56dd5
Dawid Kłos <klosdawid12@gmail.com>
2022-10-29 16:11:08 +0100

    Finish styling Bookmark component

--- Entertainment web app/ClientApp/src/components/Bookmark/Bookmark.js
+++ Entertainment web app/ClientApp/src/components/Bookmark/Bookmark.js
@@ -1,7 +1,29 @@
+import './Bookmark.scss';
+import Card from "../Card/Card";
 
 const Bookmark = () => {
     return(
-        <></>
+        <>
+            <div className="bookmark">
+                <h1 className="bookmark__title">Bookmarked Movies</h1>
+                <div className="bookmark__content">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
+            <div className="bookmark-tv">
+                <h1 className="bookmark-tv__title">Bookmarked TV Series</h1>
+                <div className="bookmark-tv__content">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
+        </>
+
     )
 }
 
