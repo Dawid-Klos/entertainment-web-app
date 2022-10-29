2c13c5adc20701b34554758589110874efc8095b
Dawid Kłos <klosdawid12@gmail.com>
2022-10-29 16:05:20 +0100

    Finish styling TVSeries component

--- Entertainment web app/ClientApp/src/components/TVSeries/TVSeries.js
+++ Entertainment web app/ClientApp/src/components/TVSeries/TVSeries.js
@@ -1,7 +1,17 @@
+import "./TVSeries.scss";
+import Card from '../Card/Card';
 
 const TVSeries = () => {
     return(
-        <></>
+        <div className="TVSeries">
+            <h1 className="TVSeries__title">TV Series</h1>
+            <div className="TVSeries__content">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
     )
 }
 
