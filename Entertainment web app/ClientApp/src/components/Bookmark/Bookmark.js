1ad3cb1daf9b62e50e8d09cdfc6d1eb24eb682ac
Dawid Klos <klosdawid12@gmail.com>
2023-08-14 14:54:34 +0100

    refactor: remove redundant code

--- Entertainment web app/ClientApp/src/components/Bookmark/Bookmark.js
+++ Entertainment web app/ClientApp/src/components/Bookmark/Bookmark.js
@@ -1,42 +1,29 @@
-import { useEffect, useState } from "react";
-import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
 
 import './Bookmark.scss';
 import Card from "../Card/Card";
-// import Spinner from '../../UI/Spinner/Spinner';
 
 const Bookmark = () => {
-
     const [loading, setLoading] = useState(true);
-    const { content } = useOutletContext();
-
-    useEffect(() => {
-        if (content !== undefined) {
-            setLoading(false)
-        }
-
-    }, [content]);
-
-    // if(loading) return <Spinner />;
-
+    
     return (
         <>
             <div className="bookmark">
                 <h1 className="bookmark__title">Bookmarked Movies</h1>
                 <div className="bookmark__content">
-                    {loading ? (<h2>Loading...</h2>) : (
-                        content.map(movie => <Card key={movie.MovieId} movie={movie} />)
-                    )
-                    }
+                    {/*{loading ? (<h2>Loading...</h2>) : (*/}
+                    {/*    content.map(movie => <Card key={movie.MovieId} movie={movie} />)*/}
+                    {/*)*/}
+                    {/*}*/}
                 </div>
             </div>
             <div className="bookmark-tv">
                 <h1 className="bookmark-tv__title">Bookmarked TV Series</h1>
                 <div className="bookmark-tv__content">
-                    {loading ? (<h2>Loading...</h2>) : (
-                        content.map(movie => <Card key={movie.MovieId} movie={movie} />)
-                    )
-                    }
+                    {/*{loading ? (<h2>Loading...</h2>) : (*/}
+                    {/*    content.map(movie => <Card key={movie.MovieId} movie={movie} />)*/}
+                    {/*)*/}
+                    {/*}*/}
                 </div>
             </div>
         </>
