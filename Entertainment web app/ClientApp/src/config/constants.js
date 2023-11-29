bf30d43c96a49dbd8abdd565833468a9a75f80a0
Dawid Klos <klosdawid12@gmail.com>
2023-11-29 12:27:07 +0000

    feat: create pages constants to be used across components

---
+++ Entertainment web app/ClientApp/src/config/constants.js
@@ -0,0 +1,24 @@
+
+
+export const pages = {
+    home: {
+        path: '/',
+        placeholder: 'Search for movies or TV series',
+        category: 'Library'
+    },
+    movies: {
+        path: '/Movies',
+        placeholder: 'Search for movies',
+        category: 'Movies'
+    },
+    tvSeries: {
+        path: '/TV-Series',
+        placeholder: 'Search for TV series',
+        category: 'TV-Series'
+    },
+    bookmarked: {
+        path: '/Bookmarked',
+        placeholder: 'Search for bookmarked movies',
+        category: 'Bookmarked'
+    }
+}
