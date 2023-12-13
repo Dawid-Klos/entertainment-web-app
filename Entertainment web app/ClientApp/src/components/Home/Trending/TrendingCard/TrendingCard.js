d6a2eaf22306fc75976e09551059b1fb154d8216
Dawid Klos <klosdawid12@gmail.com>
2023-12-13 12:12:15 +0000

    feat: use different image based on screen size in TrendingCard

--- Entertainment web app/ClientApp/src/components/Home/Trending/TrendingCard/TrendingCard.js
+++ Entertainment web app/ClientApp/src/components/Home/Trending/TrendingCard/TrendingCard.js
@@ -1,36 +1,39 @@
+import { useWindowSize} from "../../../../hooks/useWindowSize";
+
 import categoryMovieIcon from "../../../../assets/icon-category-movie.svg";
 import categoryTvIcon from "../../../../assets/icon-category-tv.svg";
 import bookmarkIcon from "../../../../assets/icon-bookmark-empty.svg";
 
 import "./TrendingCard.scss";
 
-const image = "./assets/thumbnails/1998/trending/small.jpg";
-
 const TrendingCard = ({movie}) => {
-
-    const {ImgTrendingSmall, Title, Year, Rating, Category} = movie;
+    const {isMobile} = useWindowSize();
+    const {ImgTrendingSmall, ImgTrendingLarge, Movie} = movie;
+    
+    const backgroundImg = isMobile ? ImgTrendingSmall : ImgTrendingLarge;
+    const backgroundStyles = {
+        backgroundImage: 'url(' + backgroundImg + ')',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+    }
 
     return (
-        <div className="trending-card" style={{
-            backgroundImage: 'url(' + ImgTrendingSmall + ')',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat'
-        }}>
+        <div className="trending-card" style={backgroundStyles}>
             <div className="trending-card__info">
                 <div className="trending-info">
-                    <p className="trending-info__year">{Year}</p>
+                    <p className="trending-info__year">{Movie.Year}</p>
                     <span className="trending-info__separator"></span>
                     <p className="trending-info__category">
                         <img className="trending-info__category--img"
-                             src={Category === "Movie" ? categoryMovieIcon : categoryTvIcon}
-                             alt={Category === "Movie" ? "Movie" : "TV Series"} />
-                        {Category}
+                             src={Movie.Category === "Movie" ? categoryMovieIcon : categoryTvIcon}
+                             alt={Movie.Category === "Movie" ? "Movie" : "TV Series"} />
+                        {Movie.Category}
                     </p>
                     <span className="trending-info__separator"></span>
-                    <p className="trending-info__rating">{Rating}</p>
+                    <p className="trending-info__rating">{Movie.Rating}</p>
                 </div>
-                <h2 className="trending-card__info--title">{Title}</h2>
+                <h2 className="trending-card__info--title">{Movie.Title}</h2>
             </div>
 
             <div className="trending-card__bookmark">
