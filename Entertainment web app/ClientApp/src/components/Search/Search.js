25658de7246b970083cf6fa359198af3b5f6c41d
Dawid Klos <klosdawid12@gmail.com>
2023-12-14 21:46:54 +0000

    feat: pass bookmarks to Card components in Search

--- Entertainment web app/ClientApp/src/components/Search/Search.js
+++ Entertainment web app/ClientApp/src/components/Search/Search.js
@@ -9,6 +9,9 @@ const Search = () => {
   const [title, setTitle] = useState(null);
   const searchResult = useLoaderData();
 
+  const bookmarks = searchResult.bookmarks;
+  const movies = searchResult.data;
+
   const setTitleForQuery = () => {
     setTitle(
       `Found ${searchResult.data.length} results for '${searchResult.query}' in ${searchResult.category}`,
@@ -26,8 +29,13 @@ const Search = () => {
       <h1 className="search__title">{title}</h1>
       <div className="search__content">
         {searchResult ? (
-          searchResult.data.map((movie) => (
-            <Card key={movie.MovieId} movie={movie} variant="standard" />
+          movies.map((movie) => (
+            <Card
+              key={movie.MovieId}
+              movie={movie}
+              bookmarks={bookmarks}
+              variant="standard"
+            />
           ))
         ) : (
           <p>Content has not been found</p>
