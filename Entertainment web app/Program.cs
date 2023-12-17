894c341ebe04f8da24bdfc84dc4d7eddcbd2988a
Dawid Klos <klosdawid12@gmail.com>
2023-12-17 15:57:31 +0000

    chore: add https redirection in project configuration

--- Entertainment web app/Program.cs
+++ Entertainment web app/Program.cs
@@ -115,6 +115,7 @@ if (app.Environment.IsDevelopment()) {
 else {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
 app.UseCors("CorsPolicy");
