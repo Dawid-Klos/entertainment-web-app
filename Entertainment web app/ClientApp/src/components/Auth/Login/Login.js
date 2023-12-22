3a7d017b182232937a48a646c1cd2fe9dbe4339b
Dawid Klos <klosdawid12@gmail.com>
2023-12-22 11:23:28 +0000

    style: refactor Login and Register pages styles

--- Entertainment web app/ClientApp/src/components/Auth/Login/Login.js
+++ Entertainment web app/ClientApp/src/components/Auth/Login/Login.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import logo from "../../../assets/logo.svg";
-import "./Login.scss";
+import "../Auth.scss";
 
 const Login = () => {
   const email = useRef();
@@ -40,31 +40,35 @@ const Login = () => {
   };
 
   return (
-    <section className="login">
-      <img className="login__logo" src={logo} alt="Netwix company logo" />
+    <section className="auth-section">
+      <img
+        className="auth-section__logo"
+        src={logo}
+        alt="Netwix company logo"
+      />
 
-      <div className="login__container">
+      <div className="auth-section__container">
         <form className="form" onSubmit={handleSubmit}>
           <h2 className="form__title">Login</h2>
 
           <div className="form__inputs-wrapper">
-            <div className="input">
-              <label htmlFor="email" className="input__label">
+            <div className="input-container">
+              <label htmlFor="email" className="input-container__label">
                 Email address
               </label>
               <input
-                className="input__input"
+                className="input-container__input"
                 type="text"
                 ref={email}
                 id="email"
               />
             </div>
-            <div className="input">
-              <label htmlFor="password" className="input__label">
+            <div className="input-container">
+              <label htmlFor="password" className="input-container__label">
                 Password
               </label>
               <input
-                className="input__input"
+                className="input-container__input"
                 type="password"
                 ref={password}
                 id="password"
