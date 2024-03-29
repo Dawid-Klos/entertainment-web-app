bdecae6b74888fc82828b56e0022275cb5b4b5d8
Dawid Klos <klosdawid12@gmail.com>
2024-03-29 12:34:56 +0000

    feat: style Login and Register page

---
+++ Entertainment web app/Migrations/20231203012616_UserBookmarks.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entertainmentwebapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserBookmarks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserMovie_Movies_UserMoviesMovieId",
+                table: "ApplicationUserMovie");
+
+            migrationBuilder.RenameColumn(
+                name: "UserMoviesMovieId",
+                table: "ApplicationUserMovie",
+                newName: "BookmarksMovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserMovie_Movies_BookmarksMovieId",
+                table: "ApplicationUserMovie",
+                column: "BookmarksMovieId",
+                principalTable: "Movies",
+                principalColumn: "MovieId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserMovie_Movies_BookmarksMovieId",
+                table: "ApplicationUserMovie");
+
+            migrationBuilder.RenameColumn(
+                name: "BookmarksMovieId",
+                table: "ApplicationUserMovie",
+                newName: "UserMoviesMovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserMovie_Movies_UserMoviesMovieId",
+                table: "ApplicationUserMovie",
+                column: "UserMoviesMovieId",
+                principalTable: "Movies",
+                principalColumn: "MovieId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Entertainment web app/Migrations/20231207121530_IncludeMovieInTrending.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entertainmentwebapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncludeMovieInTrending : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgSmall",
+                table: "Trending",
+                newName: "ImgTrendingSmall");
+
+            migrationBuilder.RenameColumn(
+                name: "ImgLarge",
+                table: "Trending",
+                newName: "ImgTrendingLarge");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trending_MovieId",
+                table: "Trending",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trending_Movies_MovieId",
+                table: "Trending",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "MovieId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trending_Movies_MovieId",
+                table: "Trending");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trending_MovieId",
+                table: "Trending");
+
+            migrationBuilder.RenameColumn(
+                name: "ImgTrendingSmall",
+                table: "Trending",
+                newName: "ImgSmall");
+
+            migrationBuilder.RenameColumn(
+                name: "ImgTrendingLarge",
+                table: "Trending",
+                newName: "ImgLarge");
+        }
+    }
+}
