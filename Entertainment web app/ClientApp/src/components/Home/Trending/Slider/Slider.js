0ec7c35a70e9bc8a43cb7e7224261084d113822f
Dawid Klos <klosdawid12@gmail.com>
2024-04-09 08:45:48 +0100

    feat: add breakpoints to the Slider

--- Entertainment web app/ClientApp/src/components/Home/Trending/Slider/Slider.js
+++ Entertainment web app/ClientApp/src/components/Home/Trending/Slider/Slider.js
@@ -6,24 +6,37 @@ import Card from "../../../common/Card/Card";
 const Slider = ({ movies, bookmarks }) => {
   const [sliderRef] = useKeenSlider({
     slides: {
-      perView: 2,
-      spacing: 10,
+      perView: 1.25,
+      spacing: 20,
+    },
+    breakpoints: {
+      "(min-width: 768px)": {
+        slides: {
+          perView: 2.25,
+          spacing: 30,
+        },
+      },
+      "(min-width: 1440px)": {
+        slides: {
+          perView: 3.25,
+          spacing: 30,
+        },
+      },
+      "(min-width: 1920px)": {
+        slides: {
+          perView: 3,
+          spacing: -80,
+        },
+      },
     },
   });
-
-  // TODO: Implement the breakpoint for the slider
 
   return (
     <div ref={sliderRef} className="keen-slider">
       {movies &&
         movies.map((movie) => (
-          <div className="keen-slider__slide">
-            <Card
-              key={movie.MovieId}
-              movie={movie}
-              bookmarks={bookmarks}
-              variant="trending"
-            />
+          <div className="keen-slider__slide" key={movie.MovieId}>
+            <Card movie={movie} bookmarks={bookmarks} variant="trending" />
           </div>
         ))}
     </div>
