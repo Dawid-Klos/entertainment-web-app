fbcbb9266f8e81ac9c4e302e98e76299219a0064
Dawid Klos <klosdawid12@gmail.com>
2024-04-09 07:54:47 +0100

    fix: spa proxy issues after upgrading packages

--- Entertainment web app/ClientApp/src/setupProxy.js
+++ Entertainment web app/ClientApp/src/setupProxy.js
@@ -1,35 +1,38 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { env } = require('process');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { env } = require("process");
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:52438';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+    ? env.ASPNETCORE_URLS.split(";")[0]
+    : "http://localhost:52438";
 
-const context =  [
-    "/api/Movies",
-    "/api/Movies/GetTrendingMovies",
-    "/api/Movies/GetTvSeries",
-    "/api/Auth/Register",
-    "/api/Auth/Login",
-    "/api/Auth/Logout",
-    "/api/Auth/AuthenticateUser",
-    "/api/Search/SearchByTitle",
-    "/api/Search/SearchByCategory",
-    "/api/Search/SearchByCategoryAndTitle",
-    "/api/Bookmark/GetBookmarks",
-    "/api/Bookmark/Add",
-    "/api/Bookmark/Remove",
-    "/api/Trending/GetTrending",
-    "/swagger/v1/swagger.json",
-    "/swagger",
+const context = [
+  "/api/Movies",
+  "/api/Movies/GetTrendingMovies",
+  "/api/Movies/GetTvSeries",
+  "/api/Auth/Register",
+  "/api/Auth/Login",
+  "/api/Auth/Logout",
+  "/api/Auth/AuthenticateUser",
+  "/api/Search/SearchByTitle",
+  "/api/Search/SearchByCategory",
+  "/api/Search/SearchByCategoryAndTitle",
+  "/api/Bookmark/GetBookmarks",
+  "/api/Bookmark/Add",
+  "/api/Bookmark/Remove",
+  "/api/Trending/GetTrending",
+  "/swagger/v1/swagger.json",
+  "/swagger",
 ];
 
-module.exports = function(app) {
+module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
     secure: false,
     headers: {
-      Connection: 'Keep-Alive'
-    }
+      Connection: "Keep-Alive",
+    },
   });
 
   app.use(appProxy);
