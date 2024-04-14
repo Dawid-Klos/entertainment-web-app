5c2df751b371eb0f1a31fa967aa99694b7e9695e
Dawid Klos <klosdawid12@gmail.com>
2024-04-14 18:08:41 +0100

    feat: improve hover state on card bookmark

--- Entertainment web app/ClientApp/src/assets/icons/Bookmark.js
+++ Entertainment web app/ClientApp/src/assets/icons/Bookmark.js
@@ -10,7 +10,7 @@ const Bookmark = ({ variant, className }) => {
         <path
           d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
           stroke="currentColor"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           fill="none"
         />
       ) : (
