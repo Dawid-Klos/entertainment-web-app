e4b85fffe339dc75aca005206fc8af5d3386ff91
Dawid Klos <klosdawid12@gmail.com>
2024-04-18 20:13:33 +0100

    feat: add getting trending movie by ID / add JSON responses

--- Entertainment web app/Controllers/TrendingController.cs
+++ Entertainment web app/Controllers/TrendingController.cs
@@ -12,53 +12,105 @@ namespace Entertainment_web_app.Controllers;
 [Route("api/[controller]")]
 public class TrendingController : ControllerBase
 {
-    
+
     private readonly NetwixDbContext _context;
 
     public TrendingController(NetwixDbContext context)
     {
         _context = context;
     }
-    
+
     [HttpGet]
-    [Route("[action]")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<IEnumerable<MovieDto>>> Get()
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        try {
-            var user = await _context.Users.FindAsync(userId); 
+        try
+        {
+            var user = await _context.Users.FindAsync(userId);
 
             var trendingMovieIds = await _context.Trending
                 .Select(t => t.MovieId)
                 .ToListAsync();
-            
+
             var movies = await _context.Movies
                 .Where(m => trendingMovieIds.Contains(m.MovieId))
                 .ToListAsync();
 
             var bookmarks = await _context.Bookmarks
                 .Where(b => b.ApplicationUserId == userId)
-                .Select(b => b.MovieId)
+               .Select(b => b.MovieId)
                 .ToListAsync();
 
-            var trendingMovies = movies.Select(m => new MovieDto 
-                {
-                    MovieId = m.MovieId,
-                    Title = m.Title,
-                    Year = m.Year,
-                    Category = m.Category,
-                    Rating = m.Rating,
-                    ImgSmall = m.ImgSmall,
-                    ImgMedium = m.ImgMedium,
-                    ImgLarge = m.ImgLarge,
-                    IsBookmarked = bookmarks.Contains(m.MovieId) 
-                }).ToList();
-            
-            return new JsonResult(trendingMovies); 
-        } catch (Exception ex)
-        {
-             return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred: {ex.Message}");
+            var trendingMovies = movies.Select(m => new MovieDto
+            {
+                MovieId = m.MovieId,
+                Title = m.Title,
+                Year = m.Year,
+                Category = m.Category,
+                Rating = m.Rating,
+                ImgSmall = m.ImgSmall,
+                ImgMedium = m.ImgMedium,
+                ImgLarge = m.ImgLarge,
+                IsBookmarked = bookmarks.Contains(m.MovieId)
+            }).ToList();
+
+            return new JsonResult(new { status = "success", statusCode = StatusCodes.Status200OK, data = trendingMovies });
         }
-    } 
+        catch (Exception ex)
+        {
+            return new JsonResult(new { status = "error", error = $"Internal Server Error, {ex}", statusCode = StatusCodes.Status500InternalServerError });
+        }
+    }
+
+    [HttpGet("{trendingId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<MovieDto>> Get(int trendingId)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var user = await _context.Users.FindAsync(userId);
+
+        try
+        {
+            var trendingMovie = await _context.Trending.FindAsync(trendingId);
+
+            if (trendingMovie == null)
+            {
+                return new JsonResult(new { status = "error", error = $"Trending movie with id = {trendingId} does not exist in the database", statusCode = StatusCodes.Status404NotFound });
+            }
+
+            var movie = await _context.Movies
+                .Where(m => m.MovieId == trendingMovie.MovieId)
+                .FirstOrDefaultAsync();
+
+            var bookmark = await _context.Bookmarks
+                .Where(b => b.ApplicationUserId == userId && b.MovieId == movie!.MovieId)
+                .FirstOrDefaultAsync();
+
+            var movieDto = new MovieDto
+            {
+                MovieId = movie!.MovieId,
+                Title = movie!.Title,
+                Year = movie!.Year,
+                Category = movie!.Category,
+                Rating = movie!.Rating,
+                ImgSmall = movie!.ImgSmall,
+                ImgMedium = movie!.ImgMedium,
+                ImgLarge = movie!.ImgLarge,
+                IsBookmarked = bookmark != null
+            };
+
+            return new JsonResult(new { status = "success", statusCode = StatusCodes.Status200OK, data = movieDto });
+        }
+        catch (Exception ex)
+        {
+            return new JsonResult(new { status = "error", error = $"Internal Server Error, {ex}", statusCode = StatusCodes.Status500InternalServerError });
+        }
+    }
 }
