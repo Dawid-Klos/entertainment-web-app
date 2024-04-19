1ff5a8746fa8e9328fdb6e3848ace6c9688bd072
Dawid Klos <klosdawid12@gmail.com>
2024-04-19 06:03:43 +0100

    feat: set properties of MovieDto to non-nullable

--- Entertainment web app/DTOs/MovieDto.cs
+++ Entertainment web app/DTOs/MovieDto.cs
@@ -3,13 +3,13 @@ namespace Entertainment_web_app.Models.Content;
 public class MovieDto
 {
     public int MovieId { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = null!;
     public int Year { get; set; }
-    public string Category { get; set; }
-    public string Rating { get; set; }
-    public string ImgSmall { get; set; }
-    public string ImgMedium { get; set; }
-    public string ImgLarge { get; set; }
+    public string Category { get; set; } = null!;
+    public string Rating { get; set; } = null!;
+    public string ImgSmall { get; set; } = null!;
+    public string ImgMedium { get; set; } = null!;
+    public string ImgLarge { get; set; } = null!;
     public bool IsBookmarked { get; set; }
 }
 
