fc3e5047c9fae551b8ce5a33a95a403c896024ee
Dawid Klos <klosdawid12@gmail.com>
2024-04-21 17:57:48 +0100

    feat: create MovieRepository class

---
+++ Entertainment web app/Repositories/MovieRepository.cs
@@ -0,0 +1,99 @@
+using Microsoft.EntityFrameworkCore;
+
+using Entertainment_web_app.Models.Content;
+using Entertainment_web_app.Data;
+
+namespace Entertainment_web_app.Repositories;
+
+public class MovieRepository
+{
+
+    private readonly NetwixDbContext _context;
+
+    public MovieRepository(NetwixDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        return await _context.Movies.ToListAsync();
+    }
+
+    public async Task<Movie> GetById(int movieId)
+    {
+        var movie = await _context.Movies.FindAsync(movieId);
+
+        if (movie == null)
+        {
+            throw new Exception($"Movie with ID {movieId} not found");
+        }
+
+        return movie;
+    }
+
+    public async void Add(Movie movie)
+    {
+        using var transaction = _context.Database.BeginTransaction();
+
+        try
+        {
+            _context.Movies.Add(movie);
+
+            await _context.SaveChangesAsync();
+            await transaction.CommitAsync();
+
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+
+            throw new Exception($"Error adding movie: {ex.Message}");
+        }
+    }
+
+    public async void Update(Movie movie)
+    {
+        using var transaction = _context.Database.BeginTransaction();
+
+        try
+        {
+            _context.Movies.Update(movie);
+
+            await _context.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+
+            throw new Exception($"Error updating movie: {ex.Message}");
+        }
+    }
+
+    public async void Delete(int movieId)
+    {
+        var movie = await _context.Movies.FindAsync(movieId);
+
+        if (movie == null)
+        {
+            throw new Exception($"Movie with ID {movieId} not found");
+        }
+
+        using var transaction = _context.Database.BeginTransaction();
+
+        try
+        {
+            _context.Movies.Remove(movie);
+
+            await _context.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+
+            throw new Exception($"Error deleting movie: {ex.Message}");
+        }
+    }
+}
