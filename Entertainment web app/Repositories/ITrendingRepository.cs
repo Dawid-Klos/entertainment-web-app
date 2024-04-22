8b6abd492b64ecf56c43a6fc87f6307a4f09c347
Dawid Klos <klosdawid12@gmail.com>
2024-04-22 17:06:31 +0100

    fix: ITrendingRepository using wrong Models

--- Entertainment web app/Repositories/ITrendingRepository.cs
+++ Entertainment web app/Repositories/ITrendingRepository.cs
@@ -5,9 +5,9 @@ namespace Entertainment_web_app.Repositories;
 public interface ITrendingRepository
 {
     Task<IEnumerable<Trending>> GetAll();
-    Trending GetById(int trendingId);
-    void Add(Movie trending);
-    void Update(Movie trending);
+    Task<Trending> GetById(int trendingId);
+    void Add(Trending trending);
+    void Update(Trending trending);
     void Delete(int trendingId);
 }
 
