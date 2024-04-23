7cd5757ae7345896e2b872a0a3b8032b8fdc22b0
Dawid Klos <klosdawid12@gmail.com>
2024-04-23 06:24:08 +0100

    feat: implement methods in MovieService

--- Entertainment web app/Services/MovieService.cs
+++ Entertainment web app/Services/MovieService.cs
@@ -14,7 +14,16 @@ public class MovieService : IMovieService
 
     public async Task<IEnumerable<Movie>> GetAll()
     {
-        return await _movieRepository.GetAll();
+        try
+        {
+            var movies = await _movieRepository.GetAll();
+
+            return movies;
+        }
+        catch (Exception)
+        {
+            throw new Exception("No movies found");
+        }
     }
 
     public async Task<PagedResponse<Movie>> GetAllPaginated(int pageNumber, int pageSize)
@@ -36,5 +45,97 @@ public class MovieService : IMovieService
             PageSize = pageSize,
             TotalPages = totalPages
         };
+    }
+
+    public async Task<PagedResponse<Movie>> GetByCategoryPaginated(string category, int pageNumber, int pageSize)
+    {
+        var totalMovies = _movieRepository.CountByCategory(category);
+        var totalPages = (int)Math.Ceiling(totalMovies / (double)pageSize);
+
+        if (pageNumber < 1 || pageNumber > totalPages)
+        {
+            throw new Exception("Invalid page number");
+        }
+
+        var movies = await _movieRepository.GetByCategoryPaginated(category, pageNumber, pageSize);
+
+        return new PagedResponse<Movie>
+        {
+            Data = movies.ToList(),
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalPages = totalPages
+        };
+    }
+
+    public async Task<Movie> GetById(int movieId)
+    {
+        try
+        {
+            var movie = await _movieRepository.GetById(movieId);
+
+            return movie;
+        }
+        catch (Exception)
+        {
+            throw new Exception("Movie not found");
+        }
+    }
+
+    public async void Add(Movie movie)
+    {
+        try
+        {
+            var movieExists = await _movieRepository.GetById(movie.MovieId);
+
+            if (movieExists != null)
+            {
+                throw new Exception("Movie already exists");
+            }
+
+            _movieRepository.Add(movie);
+        }
+        catch (Exception)
+        {
+            throw new Exception("Movie already exists");
+        }
+    }
+
+    public async void Update(Movie movie)
+    {
+        try
+        {
+            var movieExists = await _movieRepository.GetById(movie.MovieId);
+
+            if (movieExists == null)
+            {
+                throw new Exception("Movie not found");
+            }
+
+            _movieRepository.Update(movie);
+        }
+        catch (Exception)
+        {
+            throw new Exception("Movie does not exist");
+        }
+    }
+
+    public async void Delete(int movieId)
+    {
+        try
+        {
+            var movieExists = await _movieRepository.GetById(movieId);
+
+            if (movieExists == null)
+            {
+                throw new Exception("Movie not found");
+            }
+
+            _movieRepository.Delete(movieId);
+        }
+        catch (Exception)
+        {
+            throw new Exception("Movie does not exist");
+        }
     }
 }
