2e0e93c294aeabca008285a2e9ac8d18d14101d0
Dawid Klos <klosdawid12@gmail.com>
2024-04-28 14:24:24 +0100

    feat: add GetByCategory method to repository

--- Entertainment web app/Repositories/IMovieRepository.cs
+++ Entertainment web app/Repositories/IMovieRepository.cs
@@ -6,6 +6,7 @@ public interface IMovieRepository
 {
     Task<IEnumerable<Movie>> GetAll();
     Task<IEnumerable<Movie>> GetAllPaginated(int pageNumber, int pageSize);
+    Task<IEnumerable<Movie>> GetByCategory(string category);
     Task<IEnumerable<Movie>> GetByCategoryPaginated(string category, int pageNumber, int pageSize);
     Task<Movie> GetById(int movieId);
     void Add(Movie movie);
