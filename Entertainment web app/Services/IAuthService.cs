7cd297b774b9108e656067158a3037dcf48b2a0b
Dawid Klos <klosdawid12@gmail.com>
2024-05-04 16:24:10 +0100

    feat: replace UserService with AuthService

---
+++ Entertainment web app/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+using Entertainment_web_app.Models.Auth;
+using Entertainment_web_app.Models.User;
+using Entertainment_web_app.Models.Responses;
+
+namespace Entertainment_web_app.Services;
+
+public interface IAuthService
+{
+    Task<Response<ApplicationUser>> RegisterUserAsync(RegisterViewModel model);
+    Task<Response<ApplicationUser>> LoginUserAsync(LoginViewModel model);
+    Task<Response<ApplicationUser>> LogoutUserAsync();
+}
