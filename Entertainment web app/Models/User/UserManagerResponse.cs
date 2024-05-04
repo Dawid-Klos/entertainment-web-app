ac7067548fc63d26a9898eba3d1e6891fef60f5e
Dawid Klos <klosdawid12@gmail.com>
2024-05-04 18:00:20 +0100

    refactor: remove redundant model

--- Entertainment web app/Models/User/UserManagerResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Entertainment_web_app.Models.User;
-
-public class UserManagerResponse
-{
-    public string Message { get; set; }
-    public bool isSuccess { get; set; }
-    public IEnumerable<string> Errors { get; set; }
-    public DateTime? ExpireDate { get; set; }
-}
