a5776816364f6d88ed44f625ef9fdade6c9cb97d
Dawid Klos <klosdawid12@gmail.com>
2024-05-10 07:58:56 +0100

    feat: create generic Result class for results with data

--- Entertainment web app/Models/Responses/Result.cs
+++ Entertainment web app/Models/Responses/Result.cs
@@ -20,3 +20,26 @@ public class Result
     public static Result Success() => new Result(true, Error.None);
     public static Result Failure(Error error) => new Result(false, error);
 }
+
+public class Result<T>
+{
+    private Result(bool isSuccess, T? data, Error error)
+    {
+        if (isSuccess && error != Error.None || !isSuccess && error == Error.None)
+        {
+            throw new ArgumentException("Invalid error", nameof(error));
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+        Data = data;
+    }
+
+    public T? Data { get; }
+    public Error Error { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+
+    public static Result<T> Success(T data) => new Result<T>(true, data, Error.None);
+    public static Result<T> Failure(Error error) => new Result<T>(false, default(T), error);
+}
