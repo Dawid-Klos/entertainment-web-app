5009325728fd585bf1f72e11208c52b11ac8d76d
Dawid Klos <klosdawid12@gmail.com>
2024-05-11 14:05:45 +0100

    refactor: move DB models to Data directory

--- Entertainment web app/Services/IMovieService.cs
+++ Entertainment web app/Services/IMovieService.cs
@@ -5,10 +5,10 @@ namespace Entertainment_web_app.Services;
 
 public interface IMovieService
 {
-    Task<Result<IEnumerable<MovieDto>>> GetAll();
-    Task<Result<PagedResponse<MovieDto>>> GetAllPaginated(int pageNumber, int pageSize);
-    Task<Result<MovieDto>> GetById(int movieId);
-    Task<Result> Add(Movie movie);
-    Task<Result> Update(Movie movie);
+    Task<Result<IEnumerable<MovieDto>>> GetAll(string category);
+    Task<Result<PagedResponse<MovieDto>>> GetAllPaginated(string category, int pageNumber, int pageSize);
+    Task<Result<MovieDto>> GetById(string category, int movieId);
+    Task<Result> Add(string category, Movie movie);
+    Task<Result> Update(string category, Movie movie);
     Task<Result> Delete(int movieId);
 }
