a3538655aa71d2b73a64675aa0be1c95acf8d3df
Dawid Klos <klosdawid12@gmail.com>
2024-07-29 08:36:45 +0100

    feat: control state of HamburgerMenu from Header

--- Entertainment web app/ClientApp/src/components/Header/Header.tsx
+++ Entertainment web app/ClientApp/src/components/Header/Header.tsx
@@ -1,29 +1,46 @@
-import { useSignIn } from "@hooks/useSignIn";
-import Spinner from "@components/common/Spinner/Spinner";
+import { useState } from "react";
 
 import logo from "@assets/logo.svg";
 import avatar from "@assets/image-avatar.png";
+import MoreIcon from "@assets/icons/MoreIcon";
 
 import Nav from "./Nav/Nav";
+import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
+
 import "./Header.scss";
 
 const Header = () => {
-  const { signOut, submission } = useSignIn();
+  const [dropdownState, setDropdownState] = useState("off");
+
+  const toggleDropdown = () => {
+    if (dropdownState === "on") {
+      setDropdownState("hiding");
+
+      setTimeout(() => {
+        setDropdownState("off");
+      }, 500);
+    }
+
+    dropdownState === "off" && setDropdownState("on");
+  };
 
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Netwix company logo" />
       <Nav />
       <div className="user-info">
-        <img className="user-info__avatar" src={avatar} alt="User avatar" />
-        <button className="user-info__button" type="submit" onClick={signOut}>
-          {submission.status === "signing out" ? (
-            <Spinner loading={true} variant="primary" />
-          ) : (
-            "Logout"
-          )}
+        <button
+          className="user-info__button"
+          type="submit"
+          onClick={toggleDropdown}
+        >
+          <img className="user-info__avatar" src={avatar} alt="User avatar" />
+          <MoreIcon
+            className={`user-info__icon ${dropdownState === "on" && "user-info__icon--active"}`}
+          />
         </button>
       </div>
+      <HamburgerMenu state={dropdownState} />
     </header>
   );
 };
