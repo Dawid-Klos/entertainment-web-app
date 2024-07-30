663198c89f605d0556252810264070f124123858
Dawid Klos <klosdawid12@gmail.com>
2024-07-30 06:16:04 +0100

    feat: capture user data on auth and pass it to Layout

--- Entertainment web app/ClientApp/src/components/Layout.tsx
+++ Entertainment web app/ClientApp/src/components/Layout.tsx
@@ -1,12 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 
 import Header from "@components/Header/Header";
 import SearchBar from "@components/SearchBar/SearchBar";
+import type { User } from "@commonTypes/auth.types";
+
+type UserData = {
+  userData: User;
+};
 
 const Layout = () => {
+  const userData = useLoaderData() as UserData;
+
   return (
     <>
-      <Header />
+      <Header userInfo={userData} />
       <SearchBar />
       <Outlet />
     </>
