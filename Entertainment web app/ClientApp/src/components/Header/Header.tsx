0067cd21c8a8e1b671275b0969f2ccb8d159e9d9
Dawid Klos <klosdawid12@gmail.com>
2024-08-18 18:47:11 +0100

    feat: create useHeaderToggle hook for header logic

--- Entertainment web app/ClientApp/src/components/Header/Header.tsx
+++ Entertainment web app/ClientApp/src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
-
+import useHeaderToggle from "@hooks/useHeaderToggle";
 import logo from "@assets/logo.svg";
 import avatar from "@assets/image-avatar.png";
 import MoreIcon from "@assets/icons/MoreIcon";
@@ -15,37 +14,7 @@ type HeaderProps = {
 };
 
 const Header = ({ userInfo }: HeaderProps) => {
-  const [headerState, setHeaderState] = useState("off");
-
-  const toggleHeaderState = () => {
-    setHeaderState((prevState) => (prevState === "on" ? "hiding" : "on"));
-  };
-
-  const handleClickOutside = useCallback((event: MouseEvent) => {
-    const target = event.target as HTMLElement;
-
-    if (!target.closest(".header")) {
-      setHeaderState("hiding");
-    }
-  }, []);
-
-  useEffect(() => {
-    if (headerState === "hiding") {
-      window.removeEventListener("click", handleClickOutside);
-
-      setTimeout(() => {
-        setHeaderState("off");
-      }, 500);
-    }
-
-    if (headerState === "on") {
-      window.addEventListener("click", handleClickOutside);
-    }
-
-    return () => {
-      window.removeEventListener("click", handleClickOutside);
-    };
-  }, [headerState, handleClickOutside]);
+  const { headerState, toggleHeaderState } = useHeaderToggle();
 
   return (
     <header
